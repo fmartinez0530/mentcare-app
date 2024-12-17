0e00e24a571e371019ea56818ad864dd10aaaf51
fmartinez0530 <123339301+fmartinez0530@users.noreply.github.com>
2024-12-17 18:13:04 -0500

    Update test_therapistsurveys.py for selenium testing

--- tests/test_therapistsurveys.py
+++ tests/test_therapistsurveys.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 options = webdriver.ChromeOptions()
@@ -11,10 +11,13 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--headless")
 options.add_argument('--disable-gpu')
+options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--disable-software-rasterizer")
 
-service = Service("./chromedriver-linux64/chromedriver")
-driver = webdriver.Chrome(service=service)
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#service = Service("./chromedriver-linux64/chromedriver")
+#service = Service(ChromeDriverManager().install())
+#driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # driver_path = "E:/CS490/cs490_gp/tests/chromedriver-win64/chromedriver.exe"
 # brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
